1b159a99e2136d85d6e429a926d69b731a65b3dc
Aidan Baker <baker339@purdue.edu>
2021-05-19 11:03:29 -0400

    Building the task form

--- Tasker/Pages/CreateTask.cshtml.cs
+++ Tasker/Pages/CreateTask.cshtml.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Tasker.Pages
@@ -7,6 +8,14 @@ namespace Tasker.Pages
         public void OnGet()
         {
             
+        }
+
+        [BindProperty]
+        public Task NewTask { get; set; }
+
+        public IActionResult OnPost()
+        {
+            return RedirectToPage("Index");
         }
     }
 }
---
+++ Tasker/Task.cs
@@ -0,0 +1,10 @@
+namespace Tasker
+{
+    public class Task
+    {
+        public int Id;
+        public string Title;
+        public string Description;
+        public int Priority;
+    }
+}
