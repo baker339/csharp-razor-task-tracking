8013da4542d1a088162d3ca963ea30d6c77b4f38
Aidan Baker <baker339@purdue.edu>
2021-05-19 23:07:48 -0400

    Displaying data on the homepage

--- Tasker/Pages/Index.cshtml.cs
+++ Tasker/Pages/Index.cshtml.cs
@@ -1,12 +1,20 @@
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace Tasker.Pages
 {
     public class Index : PageModel
     {
+        public IEnumerable<Task> Tasks;
+        private DbContext _context;
+        public Index(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public void OnGet()
         {
-            
+            Tasks = _context.Tasks.OrderBy(x => x.Priority);
         }
     }
 }
